dcda183bf79def5ac587bb6d4dd9bbd9a9990584
Konstantin Kuz <stalkercatkuz@gmail.com>
2020-06-11 16:06:37 +0300

    Added objectpooler example in templates folder

---
+++ Assets/Templates/ObjectPoolerExample/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            ObjectPooler.Instance.SpawnObject("1", transform.position + Vector3.up * Random.Range(-3, 3) + Vector3.right * Random.Range(-3, 3));
+
+            yield return new WaitForSeconds(0.5f);
+            ObjectPooler.Instance.SpawnObject("2", transform.position + Vector3.up * Random.Range(-3, 3) + Vector3.right * Random.Range(-3, 3));
+
+            yield return new WaitForSeconds(0.5f);
+            ObjectPooler.Instance.SpawnObject("3", transform.position + Vector3.up * Random.Range(-3, 3) + Vector3.right * Random.Range(-3, 3));
+
+            yield return new WaitForSeconds(1f);
+            ObjectPooler.Instance.SpawnRandomObject("cubes", transform.position + Vector3.up * Random.Range(-3, 3) + Vector3.right * Random.Range(-3, 3));
+            ObjectPooler.Instance.SpawnRandomObject("cubes", transform.position + Vector3.up * Random.Range(-3, 3) + Vector3.right * Random.Range(-3, 3));
+            ObjectPooler.Instance.SpawnRandomObject("cubes", transform.position + Vector3.up * Random.Range(-3, 3) + Vector3.right * Random.Range(-3, 3));
+        }
+    }
+}
