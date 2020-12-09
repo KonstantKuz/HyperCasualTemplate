41e80844e73ed3d7d11f7bced50cb1215c6f8b8a
goodmit <dymbers@mail.ru>
2020-12-09 21:59:16 +0300

    Обновил менеджер ввода: + фикс бага, когда при управлении мышкой вместо фазы BEGAN возвращалась фаза MOVE + добавлено свойство, определяющее свайпы на дисплее (none, top, down, right или left)

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,11 +4,12 @@ public class InputManager : MonoBehaviour
 {
     [SerializeField] private bool invertVertical;
     public static TapPhase tapPhase { get; private set; }
+    public static Swipe swipe { get; private set; }
     public static Vector2 MoveVector { get; private set; }
 
     private static Vector2 _lastTapPos = Vector2.zero;
-
-#region MonoBehaviour
+    private static Vector2 _startSwipePos = Vector2.zero;
+    #region MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
@@ -28,11 +29,22 @@ public class InputManager : MonoBehaviour
     {
         MoveVector = Vector2.zero;
         tapPhase = TapPhase.NONE;
+        swipe = Swipe.NONE;
         Vector2 deltaMove = Vector2.zero;
+        Vector2 swipeDeltaMove = Vector2.zero;
 #if UNITY_STANDALONE || UNITY_EDITOR
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            tapPhase = TapPhase.BEGAN;
+            _startSwipePos = Input.mousePosition;
+            _lastTapPos = Input.mousePosition;
+        }
+
         if (Input.GetMouseButton(0))
         {
             deltaMove = (Vector2)Input.mousePosition - _lastTapPos;
+            swipeDeltaMove = (Vector2)Input.mousePosition - _startSwipePos;
 
             if (deltaMove == Vector2.zero)
             {
@@ -46,18 +58,25 @@ public class InputManager : MonoBehaviour
                     deltaMove.y = -deltaMove.y;
                 }
                 MoveVector = deltaMove.normalized;
+                if ((Mathf.Abs(deltaMove.x) > 50f || Mathf.Abs(deltaMove.y) > 50f))
+                {
+                    //Debug.Log("Swiped!!!! " + deltaMove);
+                    if (MoveVector != Vector2.zero)
+                    {
+                        swipe = GetSwipe(deltaMove);
+                    }
+                }
             }
+            
             MoveVector = deltaMove.normalized;
             _lastTapPos = Input.mousePosition;
         }
         
-        if(Input.GetMouseButtonDown(0))
-        {
-            tapPhase = TapPhase.BEGAN;
-        }
+        
         if (Input.GetMouseButtonUp(0))
         {
             tapPhase = TapPhase.ENDED;
+            _startSwipePos = Vector2.zero;
         }
 #elif UNITY_ANDROID || UNITY_IOS
         if (IsTapped())
@@ -67,12 +86,16 @@ public class InputManager : MonoBehaviour
             {
                 case TouchPhase.Began:
                     tapPhase = TapPhase.BEGAN;
+                    _startSwipePos = Input.mousePosition;
+                    _lastTapPos = Input.mousePosition;
                     break;
                 case TouchPhase.Canceled:
                     tapPhase = TapPhase.CANCELED;
+                    _startSwipePos = Vector2.zero;
                     break;
                 case TouchPhase.Ended:
                     tapPhase = TapPhase.ENDED;
+                    _startSwipePos = Vector2.zero;
                     break;
                 case TouchPhase.Moved:
                     tapPhase = TapPhase.MOVED;
@@ -82,6 +105,18 @@ public class InputManager : MonoBehaviour
                         deltaMove.y = -deltaMove.y;
                     }
                     MoveVector = deltaMove.normalized;
+                    if (MoveVector != Vector2.zero)
+                    {
+                        swipe = GetSwipe(deltaMove);
+                    }
+                    if ((Mathf.Abs(deltaMove.x) > 50f || Mathf.Abs(deltaMove.y) > 50f))
+                    {
+                        //Debug.Log("Swiped!!!! " + deltaMove);
+                        if (MoveVector != Vector2.zero)
+                        {
+                            swipe = GetSwipe(deltaMove);
+                        }
+                    }
                     break;
                 case TouchPhase.Stationary:
                     tapPhase = TapPhase.STATIONARY;
@@ -91,6 +126,35 @@ public class InputManager : MonoBehaviour
 #endif
     }
     #endregion
+
+    private Swipe GetSwipe(Vector2 move)
+    {
+        if(Mathf.Abs(move.x) > Mathf.Abs(move.y))
+        {
+            if(move.x > 0)
+            {
+                return Swipe.RIGHT;
+            }
+            else if(move.x < 0)
+            {
+                return Swipe.LEFT;
+            }
+            return Swipe.NONE;
+        }
+        else
+        {
+            if (move.y > 0)
+            {
+                return Swipe.UP;
+            }
+            else if (move.y < 0)
+            {
+                return Swipe.DOWN;
+            }
+            return Swipe.NONE;
+        }
+        
+    }
 
     public static bool IsTapped()
     {
@@ -121,4 +185,13 @@ public enum TapPhase
     STATIONARY,
     ENDED,
     CANCELED,
+}
+
+public enum Swipe
+{
+    NONE,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
 }
