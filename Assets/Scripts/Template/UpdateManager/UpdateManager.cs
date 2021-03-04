f61c0ba2a0bf440782a876a15d2a1c4ff4122f80
Konstantin Kuz <stalkercatkuz@gmail.com>
2021-03-04 14:30:36 +0300

    added update manager scripts

---
+++ Assets/Scripts/Template/UpdateManager/UpdateManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class UpdateManager : MonoBehaviour
+{
+    private void Awake()
+    {
+       Application.targetFrameRate = 60;
+    }
+    
+    private void Update()
+    {
+        for (int i = 0; i < MonoCached.customUpdates.Count; i++)
+        {
+            MonoCached.customUpdates[i].CallCustomUpdate();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < MonoCached.customFixedUpdates.Count; i++)
+        {
+            MonoCached.customFixedUpdates[i].CallCustomFixedUpdate();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        for (int i = 0; i < MonoCached.customLateUpdates.Count; i++)
+        {
+            MonoCached.customLateUpdates[i].CallCustomLateUpdate();
+        }
+    }
+}
